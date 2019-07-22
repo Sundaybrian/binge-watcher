a87ec5a38b6a843bb308fd853d1d25282205e34e
Sunday Brian <brians931@gmail.com>
2019-07-22 17:24:38 +0300

    type cast the movie id to be an interger

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ def index():
     return render_template('index.html',message=message)
 
 
-@app.route('/movie/<movie_id>')
+@app.route('/movie/<int:movie_id>')
 def movie(movie_id):
     '''
     View movie page function that returns the movie details page and its data
