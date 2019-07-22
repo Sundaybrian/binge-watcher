79062b7e56e819ecb97f974dde4157a55d29ad85
Sunday Brian <brians931@gmail.com>
2019-07-22 17:29:34 +0300

    set up the config classes

--- app/config.py
+++ app/config.py
@@ -1,1 +1,27 @@
 #where we will store our app configurations
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+class ProdConfig(Config):
+    '''
+    Production configuration class
+
+    Args:
+        Config:The parent configuration class with general configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuraton class
+
+    Args:
+        Config:The parent configuration class with general configurations settings
+    '''
+    DEBUG = True #enables debug mode in our application
+    
+        
