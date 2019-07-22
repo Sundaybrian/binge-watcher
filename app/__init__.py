36812686663f6b7a17d96e1ad29651d7d5c24351
Sunday Brian <brians931@gmail.com>
2019-07-22 17:32:55 +0300

    setup the config file and consequently updated the init file to import the devconfig class and also updating the run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 #where we will initialize our application
 
 from flask import Flask
+from .config import DevConfig
 
 #initializing application
 app = Flask(__name__)
+
+#setting up config
+
+app.config.from_object(DevConfig)
 
 
 from app import views
