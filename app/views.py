09e69d475e9561fae106c15549ad94d808c78ce5
Sunday Brian <brians931@gmail.com>
2019-07-22 17:23:41 +0300

    add dynamic routing for a specific movie

--- app/views.py
+++ app/views.py
@@ -12,4 +12,10 @@ def index():
     message="Helo there"
     return render_template('index.html',message=message)
 
-  
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template("movie.html",id=movie_id)  
