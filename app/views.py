5f7d5881d145ada9bfc6965957c08deb0abc5197
Sunday Brian <brians931@gmail.com>
2019-07-23 17:02:36 +0300

    fix the jinja flow for the index page

--- app/views.py
+++ app/views.py
@@ -9,8 +9,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'Home - Welcome to The best Movie Review Website Online'
     message="Helo there"
-    return render_template('index.html',message=message)
+    return render_template('index.html',message=message,title=title)
 
 
 @app.route('/movie/<int:movie_id>')
