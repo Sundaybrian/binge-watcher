a808ee55af12f73dd3c24ad8e963ae9adb00ae39
Sunday Brian <brians931@gmail.com>
2019-07-23 17:09:17 +0300

    update the init file to add the config file that has the api key

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 #setting up config
 
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py') #hooking up our config to the api key file
 
 
 from app import views
