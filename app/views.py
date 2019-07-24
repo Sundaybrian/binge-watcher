e138e507fbc112fee824d6b92fe99a593fc631d1
Sunday Brian <brians931@gmail.com>
2019-07-24 14:20:18 +0300

    update the movie view function to make an api call and pass the resultant data to the render template of the movie html

--- app/views.py
+++ app/views.py
@@ -31,4 +31,6 @@ def movie(movie_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template("movie.html",id=movie_id)  
+    movie = get_movie(id)
+    title = f"{movie.title}"
+    return render_template("movie.html",title=title,movie=movie)  
