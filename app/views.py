aa567ae6ffb78bf60a28d5b4284a47164ceb4fe7
Sunday Brian <brians931@gmail.com>
2019-07-24 17:16:00 +0300

    craeate the review class

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # where we will create all our view functions
 
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
 
@@ -20,10 +20,17 @@ def index():
 
 
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html',popular=popular_movies,
-    upcoming=upcoming_movie,
-    now_showing=now_showing_movie,
-    title=title)
+
+    search_movie = request.args.get('movie_query')
+    
+    if search_movie:
+        return redirect(url_for('search',movie_name = search_movie))
+    else:
+                
+        return render_template('index.html',popular=popular_movies,
+        upcoming=upcoming_movie,
+        now_showing=now_showing_movie,
+        title=title)
 
 
 @app.route('/movie/<int:id>')
