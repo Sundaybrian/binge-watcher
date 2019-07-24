edef26c405f0a5a135b8fa3cf6fba8de4f62d40f
Sunday Brian <brians931@gmail.com>
2019-07-24 18:21:43 +0300

    add amethod to get reviews in the review class

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
-from .models import reviews
+from .models import review
 from .forms import ReviewForm
 
 Review = review.Review
