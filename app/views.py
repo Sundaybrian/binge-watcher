ddff1ecbafd4ffce1c7004f6c7752225dcfe27c2
Sunday Brian <brians931@gmail.com>
2019-07-24 18:09:36 +0300

    import the revies file and the reviewform class to the views file

--- app/views.py
+++ app/views.py
@@ -3,6 +3,10 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
+from .models import reviews
+from .forms import ReviewForm
+
+Review = review.Review
 
 #views
 @app.route('/')
