abda1f7fab231e82a5123a4c9cd8c44941292368
Sunday Brian <brians931@gmail.com>
2019-07-24 13:08:41 +0300

    imported the Bootstrap from flask_bootstrap and initiliazed it

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,17 @@
 
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #initializing application
 app = Flask(__name__,instance_relative_config = True)
 
 #setting up config
-
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py') #hooking up our config to the api key file
+
+#Intiliazing Flask Extensions
+bootstrap = Bootstrap(app)
 
 
 from app import views
